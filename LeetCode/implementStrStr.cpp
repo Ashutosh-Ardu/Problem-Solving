681783589f98de892800340192e84840e422d19e
Ashutosh A <96434947+Ashutosh-Ardu@users.noreply.github.com>
2022-07-29 22:48:19 +0530

    unnecessary files deleted

--- LeetCode/implementStrStr.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-    int strStr(string haystack, string needle) {
-        if(needle.length() == 0)
-            return 0;
-        return haystack.find(needle);
-    }
-};
